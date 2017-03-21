1e9a501368d6e1ddb9c7b63524b1594d391c98a1
Chet Bortz <chet@bortz.co>
2017-03-21 19:27:15 -0400

    Initialize gem

---
+++ lib/vainglory_api.rb
@@ -0,0 +1,95 @@
+require 'json'
+require 'ostruct'
+require 'openssl'
+require 'net/http'
+
+class VaingloryAPI
+  BASE_URL = "https://api.dc01.gamelockerapp.com"
+
+  class << self
+    def status
+      get_request_without_headers(endpoint_uri('status'))
+    end
+
+    def samples(filter_params = {})
+      get_request(endpoint_uri('shards/na/samples', filter_params))
+    end
+
+    def matches(filter_params = {})
+      get_request(endpoint_uri('shards/na/matches', filter_params))
+    end
+
+    def match(match_id)
+      get_request(endpoint_uri("shards/na/matches/#{match_id}"))
+    end
+
+    def players(*names)
+      params = {"filter[playerNames]" => names.join(',')}
+      get_request(endpoint_uri('shards/na/players', params))
+    end
+
+    def player(player_id)
+      get_request(endpoint_uri("shards/na/players/#{player_id}"))
+    end
+
+    def telemetry(url)
+      get_request(URI(url), true, false)
+    end
+
+    def teams(filter_params = {})
+      raise(NotImplementedError, "Coming soon!")
+    end
+
+    def team(team_id)
+      raise(NotImplementedError, "Coming soon!")
+    end
+
+    def link(link_id)
+      raise(NotImplementedError, "Coming soon!")
+    end
+
+    private
+
+    def get_request_without_headers(uri)
+      get_request(uri, false)
+    end
+
+    def get_request(uri, with_headers = true, with_auth = true)
+      req = Net::HTTP::Get.new(uri)
+      req = apply_headers(req, with_auth) if with_headers
+
+      request(uri, req)
+    end
+
+    def apply_headers(req, with_auth = true)
+      req['Authorization'] = "Bearer #{ENV['API_KEY']}" if with_auth
+      req['X-TITLE-ID'] = "semc-vainglory"
+      req['Accept'] = "application/vnd.api+json"
+
+      req
+    end
+
+    def request(uri, req)
+      http              = Net::HTTP.new(uri.host, uri.port)
+      http.verify_mode  = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl      = true
+
+      response(http.request(req))
+    end
+
+    def endpoint_uri(path, filter_params = {})
+      uri = URI(endpoint_url(path))
+      uri.query = URI.encode_www_form(filter_params)
+
+      uri
+    end
+
+    def endpoint_url(path)
+      [BASE_URL, path].join('/')
+    end
+
+    def response(response_data)
+      JSON.parse(response_data.body, object_class: OpenStruct)
+    end
+  end
+end
